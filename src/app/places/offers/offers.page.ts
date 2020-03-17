4ed61c7ad69d32f882b1bf84fbafb540d4d2f10d
unknown <summerbasehart@gmail.com>
2020-03-17 15:14:24 -0700

    offers

--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PlacesService } from '../places.service';
+import { Place } from '../place.model';
 
 @Component({
   selector: 'app-offers',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OffersPage implements OnInit {
 
-  constructor() { }
+  loadedOffers: Place[];
+
+  constructor(private placesService: PlacesService,) { }
 
   ngOnInit() {
+    this.loadedOffers = this.placesService.getOffers();
+    console.log(this.loadedOffers)
   }
 
 }
--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -26,19 +26,47 @@ export class PlacesService {
       description: 'In Thailand',
       imageUrl: 'https://q-cf.bstatic.com/images/hotel/max1024x768/373/37342835.jpg',
       price: 58.80 
-}
+    }
   ]
+
+  offers: Place[] = [
+    {
+      id: 'p4',
+      title: 'Floating Bunglaow',
+      description: 'African East Coast',
+      imageUrl: 'https://www.smartertravel.com/uploads/2016/09/Indian-Ocean-Fish-River-Lagoon-e1502380626140.jpg',
+      price: 300.59
+    },
+    {
+      id: 'p5',
+      title: 'Old Country Home',
+      description: 'In Canada',
+      imageUrl: 'https://www.propertyguides.com/canada/media/sites/22/canada-country-home-2.jpg',
+      price: 299.59
+    },
+  ]
+
+  allPlaces: Place[] = this.offers.concat(this.places);
 
   constructor() { }
 
 
   getPlaces() {
-    return this.places
+    return this.allPlaces
   }
 
   getPlace(id: any): any {
-    const filteredPlace = this.places.filter(place => place.id == id)[0];
+    const filteredPlace = this.allPlaces.filter(place => place.id == id)[0];
     return filteredPlace;
+  }
+
+  getOffers() {
+    return this.offers
+  }
+
+  getOffer(id: any): any {
+    const filteredOffer = this.offers.filter(offer => offer.id == id)[0];
+    return filteredOffer;
   }
 
 }
