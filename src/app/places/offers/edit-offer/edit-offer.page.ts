94d43e942e56d1faf9cdaef6f6b6fddf19949c4c
unknown <summerbasehart@gmail.com>
2020-03-25 14:25:57 -0700

    functioning edit offer page

--- src/app/places/offers/edit-offer/edit-offer.page.ts
+++ src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PlacesService } from '../../places.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Place } from '../../place.model';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-offer',
@@ -12,15 +13,25 @@ export class EditOfferPage implements OnInit {
 
   offer: Place;
   offerId: string;
+  editOfferForm: FormGroup;
 
   constructor(
     private placesService: PlacesService, 
-    private activatedRoute: ActivatedRoute,  
+    private activatedRoute: ActivatedRoute, 
+    private formBuilder: FormBuilder, 
+    private router: Router 
   ) { }
 
   ngOnInit() {
     this.offerId = this.activatedRoute.snapshot.params['id']
     this.offer = this.placesService.getPlace(this.offerId);
+    this.editOfferForm = new FormGroup({
+      title: new FormControl(this.offer.title),
+      description: new FormControl(this.offer.description),      
+      imageUrl: new FormControl(this.offer.imageUrl),      
+      price: new FormControl(this.offer.price),
+      addGuestFee: new FormControl(this.offer.addGuestFee)
+    })
     console.log(this.offer)
   }
 
@@ -28,6 +39,13 @@ export class EditOfferPage implements OnInit {
     // reset page properties for proper init/enter conditions
     this.offer = undefined;
     this.offerId = undefined;
+  }
+
+  onSavePlace(){
+    // this.offer =  this.placesService.createOffer(new Place)
+    this.offerId = this.offer.id
+    this.editOfferForm = this.placesService.updateOffer(this.offerId)
+    this.router.navigate(['places/tabs/offers']);
   }
 
 }
